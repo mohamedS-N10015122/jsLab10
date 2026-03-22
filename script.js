ac407d199945eb892e37c0346571bc0616e39737
mohamedS <salad2692@gmail.com>
2026-03-21 20:26:14 -0400

    added a class

---
+++ script.js
@@ -0,0 +1,12 @@
+class ProductProperties {
+    constructor(name, price, quantity) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+
+    }
+getTotalValue(){
+    return this.price*this.quantity;
+}
+//todo: add comments
+}
