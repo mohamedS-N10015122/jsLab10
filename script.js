323592df670e8c5136b6f3d274c69a22159011df
mohamedS <salad2692@gmail.com>
2026-03-23 19:09:33 -0400

    added discount method

--- script.js
+++ script.js
@@ -11,6 +11,14 @@ getTotalValue(){
 toString(){
     return "Product: " + this.name + ", Price: " + this.price + ", Quantity: " + this.quantity;
 }
+static applyDiscount(products, discount){
+    let tempObj;
+    // delete this line when committing, TODO: follow part 3
+    for (let index = 0; index < products.length; index++) {
+    tempObj = products[index];
+    tempObj.price = (tempObj.price * discount)
+    }
+}
 //todo: add comments
 }
 class PerishableProductProperties extends ProductProperties {
@@ -22,5 +30,10 @@ class PerishableProductProperties extends ProductProperties {
     return "Product: " + this.name + ", Price: " + this.price + ", Quantity: " + this.quantity + ", Expiration Date: " + this.expirationDate;
 }
 }
-let product1 = new PerishableProductProperties("Apple", 1.99, 10, "December 14, 2026")
+let product1 = new PerishableProductProperties("Apple", 2.99, 10, "December 14, 2026")
 let product2 = new PerishableProductProperties("Banana", 1.99, 10, "March 24, 2027")
+console.log(product1);
+console.log(product2);
+ProductProperties.applyDiscount([product1,product2], 0.5)
+console.log(product1);
+console.log(product2);
