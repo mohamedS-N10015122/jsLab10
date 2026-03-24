4c30c0772c269df9d6e0e4f566d5ed117cf41c0b
mohamedS <salad2692@gmail.com>
2026-03-24 19:19:36 -0400

    added store class

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 class ProductProperties {
     constructor(name, price, quantity) {
         this.name = name;
-        this.price = price;
+        this.price = price
         this.quantity = quantity;
 
     }
@@ -13,7 +13,6 @@ toString(){
 }
 static applyDiscount(products, discount){
     let tempObj;
-    // delete this line when committing, TODO: follow part 3
     for (let index = 0; index < products.length; index++) {
     tempObj = products[index];
     tempObj.price = (tempObj.price * discount)
@@ -30,10 +29,10 @@ class PerishableProductProperties extends ProductProperties {
     return "Product: " + this.name + ", Price: " + this.price + ", Quantity: " + this.quantity + ", Expiration Date: " + this.expirationDate;
 }
 }
+class Store{
+    constructor(array) {
+        this.array = array
+    }
+}
 let product1 = new PerishableProductProperties("Apple", 2.99, 10, "December 14, 2026")
 let product2 = new PerishableProductProperties("Banana", 1.99, 10, "March 24, 2027")
-console.log(product1);
-console.log(product2);
-ProductProperties.applyDiscount([product1,product2], 0.5)
-console.log(product1);
-console.log(product2);
